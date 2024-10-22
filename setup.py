2147af694a3b44f60efb3467a390bbead72d02b5
Francesco Santini <francesco.santini@gmail.com>
2024-10-22 14:57:56 +0200

    Simplify version check

--- setup.py
+++ setup.py
@@ -15,21 +15,6 @@ def get_version():
     init_py = open(init_py_path, "r").readlines()
     version_line = [l.strip() for l in init_py if l.startswith("__version__")][0]  # noqa: E741
     version = version_line.split("=")[-1].strip().strip("'\"")
-
-    # The following is used to build release packages.
-    # Users should never use it.
-    suffix = os.getenv("VOXEL_VERSION_SUFFIX", "")
-    version = version + suffix
-    if os.getenv("BUILD_NIGHTLY", "0") == "1":
-        from datetime import datetime
-
-        date_str = datetime.today().strftime("%y%m%d")
-        version = version + ".dev" + date_str
-
-        new_init_py = [l for l in init_py if not l.startswith("__version__")]  # noqa: E741
-        new_init_py.append('__version__ = "{}"\n'.format(version))
-        with open(init_py_path, "w") as f:
-            f.write("".join(new_init_py))
     return version
 
 
@@ -151,7 +136,7 @@ setup(
     license="GNU",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
     extras_require=EXTRAS,
